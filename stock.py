5e489e719ce3e36e67142844fb912f38d8a7137f
FiLeonard <Leonard.Fischer@freenet.de>
2017-07-21 00:08:49 +0200

    update

--- stock.py
+++ stock.py
@@ -6,55 +6,42 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
-from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import linear_model
 from xgboost import XGBRegressor
-get_ipython().magic('matplotlib inline')
-
 
 start = datetime.datetime(1995, 1, 15)
 end = datetime.datetime(2017, 5, 31)
+stocks = ['AAPL', 'GOOGL', 'MSFT', 'DELL', 'GS', 'MS', 'NYSE:BAC', 'C']
+
 def get_px(stock, start, end):
-    return pdr.DataReader(stock, 'google', start, end)
-px = pd.DataFrame(get_px('AAPL', start, end))
+    return pdr.DataReader(stock, 'google', start, end)['Close']
 
-print("modify data")
-dates = np.squeeze(px.reset_index().as_matrix(columns=['Date'])).astype(dtype='datetime64[D]').reshape(-1, 1)
-prices = np.squeeze(px.as_matrix(columns=['Close']))
-print("modified data")
-
-def predict_price(dates, prices):
+def predict_price(stock):
     
-    #svr_lin = SVR(kernel= 'linear')#, C=1e3)
-    #svr_poly = SVR(kernel= 'poly')#, C=1e3, degree = 2)
-    #svr_rbf = SVR(kernel= 'rbf')#, C=1e3, gamma=0.1)
+    px = pd.DataFrame(get_px(stock, start, end)).reset_index()
+    dates = px['Date'].values.astype(dtype='datetime64[D]').reshape(-1, 1)
+    prices = px['Close'].values
+
     rfr = RandomForestRegressor(n_estimators=3, max_depth=10)
     reg = linear_model.LinearRegression()
     xgb = XGBRegressor()
     
-    print("fitting model")
-    #svr_lin.fit(dates, prices)
-    #svr_poly.fit(dates, prices)
-    #svr_rbf.fit(dates, prices)
     rfr.fit(dates, prices)
     reg.fit(dates.astype('float64'), prices)
     xgb.fit(dates.astype('float64'), prices)
-    print("models fitted")
-    
+
     plt.scatter(dates, prices, color='Black', label='Data')
-    #plt.plot(dates, svr_rbf.predict(dates), color='red', label='RBF model')
-    #plt.plot(dates, svr_poly.predict(dates), color='green', label='Poly model')
-    #plt.plot(dates, svr_lin.predict(dates), color='blue', label='Linear model')
+    
     plt.plot(dates, rfr.predict(dates), color='red', label='RFC model')
     plt.plot(dates, reg.predict(dates.astype('float64')), color='blue', label='BREG model')
     plt.plot(dates, xgb.predict(dates.astype('float64')), color='green', label='XGB')
     
     plt.xlabel('Date')
     plt.ylabel('Price')
-    plt.title('Support Vector Regression/RFR')
+    plt.title('Support Vector Regression/RFR for %s' % stock)
     plt.legend()
     plt.show()
 
-
-predict_price(dates, prices)
+for n in stocks:
+    predict_price(n)
